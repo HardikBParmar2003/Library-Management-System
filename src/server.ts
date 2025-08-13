f14187f37c7e1c1468c05db0eba001934a7931a7
Hardik Parmar <hardik.parmar@esparkbizmail.com>
2025-08-13 15:11:22 +0530

    feature:worker-service added

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import "./worker/emailWorker";
+import "./worker/dueDateWorker";
+import "./worker/reminderWorker"
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`📧 Worker running on http://localhost:${port}`);
+});
